f37b76ba40df1c47ae42ae15ebfbca5224b5cf5e
Po-wei Huang <poweihuang17@gmail>
2018-02-11 14:42:42 +0800

    Finish 316. It's not hard as I though. It also uses some concepts of locality. Start to work on 321... It seems to be really hard...

--- Leetcode/Greedy/316_remove_duplicate_letters.py
+++ Leetcode/Greedy/316_remove_duplicate_letters.py
@@ -1,3 +1,5 @@
+# Question: What's ascending stack?
+
 class Solution(object):
     def removeDuplicateLetters(self, s):
         """
@@ -5,3 +7,20 @@ class Solution(object):
         :rtype: str
         """
         
+        remaining=collections.defaultdict(int)
+        
+        for c in s:
+            remaining[c]+=1
+        
+        in_stack, stack=set(), []
+        
+        for c in s:
+            if c not in in_stack:
+                while stack and stack[-1]>c and remaining[stack[-1]]>0:
+                    in_stack.remove(stack.pop())
+            
+                stack+=c
+                in_stack.add(c)
+            remaining[c]-=1
+        
+        return ''.join(stack)
