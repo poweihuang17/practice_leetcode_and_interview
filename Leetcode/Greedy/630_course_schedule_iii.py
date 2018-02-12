399bc40f47b055031222ed5302dd8ca165384d7c
Po-wei Huang <poweihuang17@gmail>
2018-02-12 14:04:32 +0800

    Finish 630. It's an operational model. Looks plausible. Takes the fattest intervals. But I still not sure how to prove it. I guess sometimes we need to write an approximate algorithm.

--- Leetcode/Greedy/630_course_schedule_iii.py
+++ Leetcode/Greedy/630_course_schedule_iii.py
@@ -1,3 +1,4 @@
+import heapq
 class Solution(object):
     def scheduleCourse(self, courses):
         """
@@ -5,3 +6,18 @@ class Solution(object):
         :rtype: int
         """
         
+        courses.sort(key=lambda(t, end): end)
+
+        max_heap=[]
+        now=0
+
+        for course in courses:
+        	now+=course[0]
+        	heapq.heappush(max_heap, -course[0])
+        	if now>course[1]:
+        		now+=heapq.heappop(max_heap)
+
+        return len(max_heap)
+
+s=Solution()
+print s.scheduleCourse([[5,5],[4,6],[2,6]])
