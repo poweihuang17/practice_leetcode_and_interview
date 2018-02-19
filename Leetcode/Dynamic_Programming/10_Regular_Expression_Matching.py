a62384a1ab114c2d92a6c82fda01ce2b571ebbb8
Po-wei Huang <poweihuang17@gmail>
2018-02-20 00:26:58 +0800

    Finish problem 10. It's am challenging problem The case for multiple replacement is not so convincing to me, but I think it's somewhat correct. Need more thought later.

---
+++ Leetcode/Dynamic_Programming/10_Regular_Expression_Matching.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+
+        table=[[False]*(len(s)+1) for i in range(len(p)+1)]
+
+        table[0][0]=True
+
+        for i in range(1,len(s)+1):
+        	table[0][i]=False
+
+        for i in range(1,len(p)+1):
+        	table[i][0]=i>1 and '*'==p[i-1] and table[i-2][0]
+
+       	for i in range(1, len(p)+1):
+       		for j in range(1,len(s)+1):
+       			if s[j-1]==p[i-1]:
+       				table[i][j]=table[i-1][j-1]
+       			elif p[i-1]=='.':
+       				table[i][j]=table[i-1][j-1]
+       			elif p[i-1]=='*':
+       				if p[i-2]=='.' or p[i-2]==s[j-1]:
+       					table[i][j]=table[i][j-1] or table[i-2][j-1] or table[i-2][j]
+       				elif p[i-2]!=s[j-1]:
+       					table[i][j]=table[i-2][j]
+       	
+       	for i in range(len(p)+1):
+       		print table[i]
+		
+       	return table[len(p)][len(s)]
+s=Solution()
+#print s.isMatch("aa","a")
+#print s.isMatch("aa","aa")
+#print s.isMatch("aaa","aa")
+#print s.isMatch("aa", "a*")
+print s.isMatch("aa", ".*")
+print s.isMatch("ab", ".*")
+#print s.isMatch("aab", "c*a*b")
+
+#print s.isMatch("aaa","ab*ac*a")
