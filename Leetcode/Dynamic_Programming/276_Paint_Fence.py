a51ea6961c69c66384cae285d7dc626cdd4b2b9a
Po-wei Huang <poweihuang17@gmail>
2018-02-23 17:31:19 +0800

    Finish 276. Just a typo and now it's fixed and accepted by leetcode. Previously, the code was not tested because the problem is locked.

--- Leetcode/Dynamic_Programming/276_Paint_Fence.py
+++ Leetcode/Dynamic_Programming/276_Paint_Fence.py
@@ -1,22 +1,24 @@
 class Solution(object):
 	def numWays(self, n, k):
 		"""
-        :type n: int
-        :type k: int
-        :rtype: int
-        """
-        if n<=0 or k<=0:
-        	return 0
-        if n==1:
-        	return k
-        elif n==2:
-        	return k*k
+		:type n: int
+		:type k: int
+		:rtype: int
+		"""
+		if n<=0 or k<=0:
+			return 0
+		if n==1:
+			return k
+		elif n==2:
+			return k*k
 
-        table=[(0,0)]*n
-        table[0]=(k,k)
-        table[1]=(k,k*(k-1))
+		table=[(0,0)]*n
+		table[0]=(k,k)
+		table[1]=(k,k*(k-1))
 
-        for i in range(2,n):
-        	table[i]=(table[i][1], table[i][1]*(k-1)+table[i][0]*(k-1))
+		for i in range(2,n):
+			table[i]=(table[i-1][1], table[i-1][1]*(k-1)+table[i-1][0]*(k-1))
+		#print table
+		return table[n-1][0]+table[n-1][1]
 
-        return table[n-1][0]+table[n-1][1]
+print Solution().numWays(3,2)
