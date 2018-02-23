23a28ef7ed3dfebec501c5ebba9ea724a5828de7
Po-wei Huang <poweihuang17@gmail>
2018-02-23 12:59:19 +0800

    Finish 17. Start to work on backtracking, as I'm not familiar with backtracking.

--- Leetcode/Backtracking/17_Letter_Combinations_of_a_Phone_Number.py
+++ Leetcode/Backtracking/17_Letter_Combinations_of_a_Phone_Number.py
@@ -1,6 +1,20 @@
+# Question : How to avoid copy across function callls in python?
 class Solution(object):
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
+
+        digits_map={"2":"abc", "3":"def","4":"ghi","5":"jkl","6":"mno",
+        "7":"pqrs","8":"tuv","9":"wxyz","0":""}
+
+        if digits=="":
+        	return []
+        elif len(digits)==1:
+        	return list(digits_map[digits])
+
+        return [s+c for s in self.letterCombinations(digits[:-1]) for c in digits_map[digits[-1]]]
+
+print Solution().letterCombinations("23")
+        	
