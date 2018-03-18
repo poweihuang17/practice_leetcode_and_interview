b6851794846f4c92a3418181879ee766240ef76f
Po-wei Huang <poweihuang17@gmail.com>
2018-03-18 15:25:24 +0800

    Finish 367.Remember to use long to prevent overflow problem.

---
+++ Leetcode/Binary_Search/367_Valid_Perfect_Square.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low=1, high=num;
+        while(low<=high)
+        {
+            long mid=(low+high)/2;
+        	if(mid*mid ==num){
+        		return true;
+        	}
+        	else if (mid*mid < num)
+        	{
+        		low=mid+1;
+        	}
+        	else
+        	{
+        		high=mid-1;
+        	}
+        }
+        return false;
+    }
+};
