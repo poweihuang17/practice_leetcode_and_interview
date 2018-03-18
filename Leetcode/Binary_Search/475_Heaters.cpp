18bb6c537dcece9b6b29bd6e880d0880d1f7729e
Po-wei Huang <poweihuang17@gmail.com>
2018-03-18 15:45:37 +0800

    Finish 475. RHS with LHS could do the work.

---
+++ Leetcode/Binary_Search/475_Heaters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findRadius(vector<int>& houses, vector<int>& heaters) {
+        sort(houses.begin(),houses.end());
+        sort(heaters.begin(),heaters.end());
+        vector<int> res(houses.size(), INT_MAX);
+
+        for(int i=0,h=0; i< houses.size() && h<heaters.size();)
+        {
+        	if(houses[i]<=heaters[h]) {
+        		res[i]=heaters[h]-houses[i];
+        		i++;
+        	}
+        	else
+        		h++;
+        }
+        for(int i=houses.size()-1, h=heaters.size()-1; i>=0 && h>=0;)
+        {
+        	if(houses[i]>=heaters[h]){
+        		res[i]=min(res[i],houses[i]-heaters[h]);
+        		i--;
+        	}
+        	else
+        		h--;
+        	
+        }
+        return *max_element(res.begin(),res.end());
+    }
+};
