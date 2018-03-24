b3882cdf91fac640e56cdda66b4a14d544e20089
Po-wei Huang <poweihuang17@gmail.com>
2018-03-24 15:37:45 +0800

    Finish 116. It's the same that problem of right side view.

---
+++ Leetcode/Tree/116_Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        vector<TreeLinkNode *> rightside={};
+        recursion(root, 1, rightside);
+        return;
+    }
+
+    void recursion(TreeLinkNode * node, int level, vector<TreeLinkNode * > & rightside)
+    {
+    	if(node==NULL) return ;
+    	if(level>rightside.size())
+    	{
+    		node->next=NULL;
+    		rightside.push_back(node);
+    	}
+    	else
+    	{
+    		node->next=rightside[level-1];
+    		rightside[level-1]=node;
+    	}
+
+    	if(node->right)
+    		recursion(node->right, level+1, rightside);
+    	if(node->left)
+    		recursion(node->left,level+1, rightside);
+
+    }
+};
