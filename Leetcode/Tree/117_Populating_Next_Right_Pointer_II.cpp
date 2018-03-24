7a125bf7cffeccf0cfd96d68762f5052d44445cf
Po-wei Huang <poweihuang17@gmail.com>
2018-03-24 15:44:53 +0800

    Finish 117. O(n) space method again... Need to think about method with O(1) space.

---
+++ Leetcode/Tree/117_Populating_Next_Right_Pointer_II.cpp
@@ -0,0 +1,44 @@
+/*
+Same as 116.
+Fuck, this solution use O(logn) spaces....
+Need to use O(1) space....
+Need to think about it later.
+*/
+
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        vector<TreeLinkNode *> rightside={};
+        recursion(root, 1, rightside);
+        return;
+    }
+
+    void recursion(TreeLinkNode * node, int level, vector<TreeLinkNode * > & rightside)
+    {
+    	if(node==NULL) return ;
+    	if(level>rightside.size())
+    	{
+    		node->next=NULL;
+    		rightside.push_back(node);
+    	}
+    	else
+    	{
+    		node->next=rightside[level-1];
+    		rightside[level-1]=node;
+    	}
+
+    	if(node->right)
+    		recursion(node->right, level+1, rightside);
+    	if(node->left)
+    		recursion(node->left,level+1, rightside);
+
+    }
+};
