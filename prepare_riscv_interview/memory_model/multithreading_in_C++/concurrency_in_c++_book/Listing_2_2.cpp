ff1f008cbf20b02c149e8ca626ed349dd3d96fbb
Po-wei Huang <poweihuang17@gmail.com>
2018-03-25 16:10:30 +0800

    Check in half-version of 2.2.

---
+++ prepare_riscv_interview/memory_model/multithreading_in_C++/concurrency_in_c++_book/Listing_2_2.cpp
@@ -0,0 +1,6 @@
+//The previous code will induce segmentation.
+//Need to use try/except and join to fix it.
+//Need to learn try/except, join, and detach in this code.
+#include <iostream>
+#include <thread>
+using namespace std;
