509456a09a86fece079e095272054ef767faca6c
Po-wei Huang <poweihuang17@gmail.com>
2018-03-29 18:50:27 +0800

    Finish 647.

---
+++ Leetcode/Dynamic_Programming/647_Palindromic_Substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int countSubstrings(string s) {
+        counter=0;
+        for(int i=0;i<s.size();i++)
+        {
+            extend_odd(s,i);
+            extend_even(s,i);
+        }
+        return counter;
+    }
+    void extend_odd(string s, int i)
+    {
+        int k=0;
+        while(i-k>=0 && i+k<s.size() && s[i+k]==s[i-k])
+        {
+            counter++;
+            k++;
+        }
+    }
+    
+    void extend_even(string s, int i)
+    {
+        int k=0;
+        while(i+k+1<s.size() && i-k>=0 && s[i+k+1]==s[i-k])
+        {
+            counter++;
+            k++;
+        }
+    }
+
+private:
+        int counter;
+    
+};
