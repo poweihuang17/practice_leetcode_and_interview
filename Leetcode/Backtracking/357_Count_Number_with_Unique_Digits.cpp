182dab17055c3aa7631b4893066c654f907778c6
Po-wei Huang <poweihuang17@gmail.com>
2018-04-01 16:30:52 +0800

    Finish 357. Don't stick to the boring coding template. Please learn how to do different backtracking from this solution.

---
+++ Leetcode/Backtracking/357_Count_Number_with_Unique_Digits.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n>10)
+        	return countNumbersWithUniqueDigits(10);
+
+    	int count=1;
+    	long max=(long) pow(10,n);
+
+    	vector<bool> visited(10,false);
+
+        for(int i=1;i<10;i++)
+        {
+            visited[i]=true;
+            backtrack(visited,i, max,count);
+            visited[i]=false;
+        }
+        
+        return count;
+    }
+
+    void backtrack(vector<bool> & visited, int number, int max, int  & count)
+    {
+    	if(number<max)
+    		count++;
+    	else
+    		return;
+
+    	for(int i=0;i<10;i++)
+    	{
+    		if(visited[i]==true) continue;
+    		visited[i]=true;
+    		backtrack(visited,number*10+i,max, count);
+    		visited[i]=false;
+    	}
+    }
+};
