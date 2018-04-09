c12ed8234dcbe25b47c63800755cd64666e1e35b
Po-wei Huang <poweihuang17@gmail.com>
2018-04-09 14:13:40 +0800

    Finish 536.

---
+++ Leetcode/Tree/536_Construct_Binary_Tree_from_String.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* str2tree(string s) {
+    	int i=0;
+    	return s.size()==0? nullptr:build(s,i);
+    }
+
+private:
+	TreeNode * build(string &s, int &i)
+	{
+		int start=i;
+		if(s[i]=='-')
+		{
+			i++;
+		}
+
+		while(isdigit(s[i]))
+		{
+			i++;
+		}
+		int num=stoi(s.substr(start,i-start));
+		TreeNode * node= new TreeNode(num);
+		if(s[i]=='(')
+		{
+			node->left=build(s,++i);
+			i++;
+		}
+		if(s[i]=='(')
+		{
+			node->right=build(s,++i);
+			i++;
+		}
+		return node;
+	}
+};
