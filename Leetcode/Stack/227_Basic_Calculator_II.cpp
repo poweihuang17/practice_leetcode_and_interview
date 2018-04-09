8b8060a21a2037f85b4daef45d92f748b264269d
Po-wei Huang <poweihuang17@gmail.com>
2018-04-09 13:32:49 +0800

    Finish 227. It's simpler than 224.

---
+++ Leetcode/Stack/227_Basic_Calculator_II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int calculate(string s) {
+        istringstream in ('+'+s+'+');
+        long long total=0, term=0,n;
+        char op;
+
+        while(in>>op)
+        {
+        	if(op=='+' || op=='-')
+        	{
+        		total+=term;
+        		in>>term;
+        		term *= (op=='+'? 1:-1);
+        	}
+        	else
+        	{
+        		in>>n;
+        		if(op=='*')
+        			term*=n;
+        		else
+        			term/=n;
+        	}
+        }
+        return total;
+    }
+};
