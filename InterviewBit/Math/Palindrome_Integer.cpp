9c478a3ab1ce15596bea69bbcc8b6b6b70462a88
Po-wei Huang <poweihuang17@gmail.com>
2018-04-21 06:51:39 +0800

    Finish Palindrome Number problem in InterviewBit.

---
+++ InterviewBit/Math/Palindrome_Integer.cpp
@@ -0,0 +1,10 @@
+int Solution::isPalindrome(int A) {
+    if((A!=0 && A%10==0) || A<0) return false;
+    int x=0;
+    while(A>x)
+    {
+        x=x*10+A%10;
+        A/=10;
+    }
+    return x==A || A==x/10;
+}
