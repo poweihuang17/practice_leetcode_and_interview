d31e2cf794026c11131d8199c160513b144e6f77
Po-wei Huang <poweihuang17@gmail.com>
2018-04-21 07:12:58 +0800

    Finish Rearrange Array.

---
+++ InterviewBit/Math/Rearrange_Array.cpp
@@ -0,0 +1,16 @@
+void Solution::arrange(vector<int> &A) {
+    // Do not write main() function.
+    // Do not read input, instead use the arguments to the function.
+    // Do not print the output, instead return values as specified
+    // Still have a doubt. Checkout www.interviewbit.com/pages/sample_codes/ for more details
+    for(int i=0;i<A.size();i++)
+    {
+        A[i]=(A[A[i]]%A.size())*A.size()+A[i];
+    }
+    for(int i=0;i<A.size();i++)
+    {
+        A[i]/=A.size();
+    }
+    
+    return ;
+}
