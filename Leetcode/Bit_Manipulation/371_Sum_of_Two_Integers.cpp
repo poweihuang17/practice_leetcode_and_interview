46f6a578b432628e92351a2c315f93c0ee04714d
Po-wei Huang <poweihuang17@gmail.com>
2018-04-21 08:05:54 +0800

    Update important links for bit manipulation in 371.

--- Leetcode/Bit_Manipulation/371_Sum_of_Two_Integers.cpp
+++ Leetcode/Bit_Manipulation/371_Sum_of_Two_Integers.cpp
@@ -4,3 +4,4 @@ public:
 int getSum(int a, int b) {
     return b==0? a:getSum(a^b, (a&b)<<1);} 
 };
+//https://leetcode.com/problems/sum-of-two-integers/discuss/84278/A-summary:-how-to-use-bit-manipulation-to-solve-problems-easily-and-efficiently
