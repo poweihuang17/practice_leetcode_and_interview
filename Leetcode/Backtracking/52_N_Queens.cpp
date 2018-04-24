67aaf11b8ee1f5066b0605020697c88bd37a10e5
Po-wei Huang <poweihuang17@gmail.com>
2018-04-24 14:34:32 +0800

    Finish 52.

---
+++ Leetcode/Backtracking/52_N_Queens.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int res = 0;
+        vector<int> pos(n, -1);
+        totalNQueensDFS(pos, 0, res);
+        return res;
+    }
+    void totalNQueensDFS(vector<int> &pos, int row, int &res) {
+        int n = pos.size();
+        if (row == n) ++res;
+        else {
+            for (int col = 0; col < n; ++col) {
+                if (isValid(pos, row, col)) {
+                    pos[row] = col;
+                    totalNQueensDFS(pos, row + 1, res);
+                    pos[row] = -1;
+                }
+            }
+        }
+    }
+    bool isValid(vector<int> &pos, int row, int col) {
+        for (int i = 0; i < row; ++i) {
+            if (col == pos[i] || abs(row - i) == abs(col - pos[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
