419ee1661ac9f7fb4f55109003456358bb0663b6
Po-wei Huang <poweihuang17@gmail.com>
2018-04-24 10:13:18 +0800

    Finish 318.

---
+++ Leetcode/Bit_Manipulation/318_Maximum_Product_of_Word_Length.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProduct(vector<string>& words) {
+        int res = 0;
+        vector<int> mask(words.size(), 0);
+        for (int i = 0; i < words.size(); ++i) {
+            for (char c : words[i]) {
+                mask[i] |= 1 << (c - 'a');
+            }
+            for (int j = 0; j < i; ++j) {
+                if (!(mask[i] & mask[j])) {
+                    res = max(res, int(words[i].size() * words[j].size()));
+                }
+            }
+        }
+        return res;
+    }
+};
