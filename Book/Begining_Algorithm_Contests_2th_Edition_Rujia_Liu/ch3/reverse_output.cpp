3a0e7cb300c94ea1f6ef45908d56b2995a873e6d
Po-wei Huang <poweihuang17@gmail.com>
2018-04-26 20:23:19 +0800

    Start to work on Liu's book.

---
+++ Book/Begining_Algorithm_Contests_2th_Edition_Rujia_Liu/ch3/reverse_output.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define maxn 105
+int a[maxn];
+int main()
+{
+	int x,n=0;
+	while(scanf("%d",&x)==1)
+		{
+		a[n++]=x;
+		}
+	for(int i=n-1;i>=1;i--)
+		{
+		printf("%d ",a[i]);
+		}
+	printf("%d\n",a[0]);
+	return 0;
+}
